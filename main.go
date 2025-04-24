9788dfab0693e76c08c862c623c583e2aff14bef
samznd <samzandian99@gmail.com>
2025-04-24 14:01:40 +0330

    feat: cli ux improvements

--- main.go
+++ main.go
@@ -14,7 +14,16 @@ var RootCmd = &cobra.Command{
 }
 
 func main() {
+	RootCmd.SetHelpCommand(&cobra.Command{Hidden: true})
+	RootCmd.SetFlagErrorFunc(func(cmd *cobra.Command, err error) error {
+		fmt.Printf("❌ %v\n\n", err)
+		cmd.Usage()
+		os.Exit(1)
+		return nil
+	})
+
 	RootCmd.AddCommand(generator.InitCmd)
+
 	if err := RootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
